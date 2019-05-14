cf6aa6ecef2e5038b5919228be74877c5ebaca0f
Douglas Markussen <doug@graspen.com>
2019-05-14 13:39:03 -0500

    Updated copyright dates

--- src/core.h
+++ src/core.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2014-2016 The ShadowCoin developers
-// Copyright (c) 2016-2018 ARMR developers
+// Copyright (c) 2018-2019 ARMR developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
