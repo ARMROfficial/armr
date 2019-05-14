cddfbe2334052aed61368ac616cf55fc271acda3
Douglas Markussen <doug@graspen.com>
2019-05-14 18:39:57 -0500

    Added CAnonOutputCount and CAnonOutput classes

--- src/core.cpp
+++ src/core.cpp
@@ -4,5 +4,22 @@
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
 #include "core.h"
-#include "core.h"
+
+void addAnonOutput(CPubKey& pkAo, CAnonOutput& anonOutput, txMixins_container& txMixinsContainer)
+{
+    // create pair with vout index of tx and pubKey
+    const auto & pairOutPubkey = std::make_pair(anonOutput.outpoint.n, pkAo);
+    // check if CTxMixins does already exist in container for tx hash
+    const auto & txHashIndex = txMixinsContainer.get<TXHASH>();
+    const auto it = txHashIndex.find(anonOutput.outpoint.hash);
+    if (it != txHashIndex.end())
+        // add anon pubKey to existing CTxMixins in container
+        it->vOutPubKeys.push_back(pairOutPubkey);
+    else
+    {
+        CTxMixins txMixins(anonOutput.outpoint.hash);
+        txMixins.vOutPubKeys.push_back(pairOutPubkey);
+        txMixinsContainer.push_back(txMixins);
+    }
+}
 
