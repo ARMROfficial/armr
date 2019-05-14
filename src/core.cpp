5ad2271a7d4473220832a21661278b5f78a252b9
Douglas Markussen <doug@graspen.com>
2019-05-14 10:08:49 -0500

    Created blank core files

---
+++ src/core.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2014-2016 The ShadowCoin developers
+// Copyright (c) 2018-2019 ARMR developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+#include "core.h"
+#include "core.h"
+
---
+++ src/core.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2014-2016 The ShadowCoin developers
+// Copyright (c) 2016-2018 ARMR developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+
+#ifndef CORE_H
+#define CORE_H
+
+
+
+#endif // CORE_H
