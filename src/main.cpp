451c2c961c16a1bf875e97c8d13ba47e59a555c6
Godlove Damian <godlove@innocent.com>
2019-05-16 03:11:57 +0300

    Added first reward reduction for staking

--- src/main.cpp
+++ src/main.cpp
@@ -1264,6 +1264,7 @@ int64_t GetProofOfWorkReward(int nHeight, int64_t nFees, const CBlockIndex* pind
 }
 
 static const int ARMR_GIFT_POS_BLOCK_COUNT = 168440;
+static const int FIRST_REDUCTION_POS_BLOCK_COUNT = 324022;
 static const int YEARLY_POS_BLOCK_COUNT = 525600;
 
 int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
