0be854c4873c72655e7e2ce70ee494f751c993d2
Godlove Damian <godlove@innocent.com>
2019-05-16 03:19:07 +0300

    Added flat staking reward

--- src/main.cpp
+++ src/main.cpp
@@ -1267,6 +1267,7 @@ static const int ARMR_GIFT_POS_BLOCK_COUNT = 168440;
 static const int FIRST_REDUCTION_POS_BLOCK_COUNT = 324022;
 static const int CHRISTMAS_GIFT_POS_BLOCK_COUNT = 401170;
 static const int FINAL_REDUCTION_POS_BLOCK_COUNT = 479604;
+static const int FLAT_POS_BLOCK_COUNT = 555467;
 static const int YEARLY_POS_BLOCK_COUNT = 525600;
 
 int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
