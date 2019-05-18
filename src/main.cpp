d0840da02f8c0decb49b6c86088b3e6215192d7a
Godlove Damian <godlove@innocent.com>
2019-05-18 18:50:43 +0300

    Printlog 15% POS rewards

--- src/main.cpp
+++ src/main.cpp
@@ -1284,6 +1284,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
     else if (nPoSHeight < FIRST_REDUCTION_POS_BLOCK_COUNT)
     {
         nSubsidy = 15 * nRewardCoinYear * nCoinAge / 365;
+        LogPrintf("calculating 15% reward");
     }
     else if (nPoSHeight < CHRISTMAS_GIFT_POS_BLOCK_COUNT)
     {
