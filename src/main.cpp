133d1b20d46af821be3377395e6580006c7e7735
Godlove Damian <godlove@innocent.com>
2019-05-25 23:44:57 +0300

    adjusted target block for christmas gift

--- src/main.cpp
+++ src/main.cpp
@@ -1265,7 +1265,7 @@ int64_t GetProofOfWorkReward(int nHeight, int64_t nFees, const CBlockIndex* pind
 
 static const int ARMR_GIFT_POS_BLOCK_COUNT = 6000;
 static const int FIRST_REDUCTION_POS_BLOCK_COUNT = 6030;
-static const int CHRISTMAS_GIFT_POS_BLOCK_COUNT = 5530;
+static const int CHRISTMAS_GIFT_POS_BLOCK_COUNT = 6060;
 static const int FINAL_REDUCTION_POS_BLOCK_COUNT = 5560;
 static const int FLAT_POS_BLOCK_COUNT = 5590;
 static const int YEARLY_POS_BLOCK_COUNT = 5620;
