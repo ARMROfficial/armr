88e1f1bcb086ac1da3030b0d34aae7cf31caa031
Godlove Damian <godlove@innocent.com>
2019-05-27 00:39:01 +0300

    removed log print 5%

--- src/main.cpp
+++ src/main.cpp
@@ -1287,7 +1287,6 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
     else if (nHeight < CHRISTMAS_GIFT_POS_BLOCK_COUNT)
     {
         nSubsidy = 5 * nRewardCoinYear * nCoinAge / 365;
-        LogPrintf("calculating 5% reward\n");
     }
     else if (nHeight < FINAL_REDUCTION_POS_BLOCK_COUNT)
     {
