03d725dc1e8c635891ec862f7f6839e08f7b8da6
Godlove Damian <godlove@innocent.com>
2019-05-27 00:31:36 +0300

    updated flat POS block

--- src/main.cpp
+++ src/main.cpp
@@ -1267,7 +1267,7 @@ static const int ARMR_GIFT_POS_BLOCK_COUNT = 184814;
 static const int FIRST_REDUCTION_POS_BLOCK_COUNT = 324022;
 static const int CHRISTMAS_GIFT_POS_BLOCK_COUNT = 433877;
 static const int FINAL_REDUCTION_POS_BLOCK_COUNT = 482989;
-static const int FLAT_POS_BLOCK_COUNT = 6120;
+static const int FLAT_POS_BLOCK_COUNT = 559241;
 static const int YEARLY_POS_BLOCK_COUNT = 6150;
 
 int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
