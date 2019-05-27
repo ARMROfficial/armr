2fce7d3cced3cd26d90333ccb0821f2d69b72db2
dmarkussen <doug@graspen.com>
2019-05-26 21:40:37 -0500

    Updated FIRST_REDUCTION_POS_BLOCK_COUNT value to mainnet estimated block number
    Ending the 15% stake reward and reducing it to 5% with an estimated date of October 1st, 2019.  Block number 324022 should be in the ballpark

--- src/main.cpp
+++ src/main.cpp
@@ -1264,7 +1264,7 @@ int64_t GetProofOfWorkReward(int nHeight, int64_t nFees, const CBlockIndex* pind
 }
 
 static const int ARMR_GIFT_POS_BLOCK_COUNT = 184814;
-static const int FIRST_REDUCTION_POS_BLOCK_COUNT = 6030;
+static const int FIRST_REDUCTION_POS_BLOCK_COUNT = 324022;
 static const int CHRISTMAS_GIFT_POS_BLOCK_COUNT = 6060;
 static const int FINAL_REDUCTION_POS_BLOCK_COUNT = 6090;
 static const int FLAT_POS_BLOCK_COUNT = 6120;
