5da520df067c3074875f33b42b6f3386a7b97ddd
Godlove Damian <godlove@innocent.com>
2019-05-28 20:47:58 +0300

    removed inflation on POS gift block calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1282,7 +1282,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
     }
     else if (nHeight < FIRST_REDUCTION_POS_BLOCK_COUNT)
     {
-        nSubsidy = 15 * nRewardCoinYear * nCoinAge / 365;
+        nSubsidy = 0.15 * nCoinAge / 365;
     }
     else if (nHeight < CHRISTMAS_GIFT_POS_BLOCK_COUNT)
     {
