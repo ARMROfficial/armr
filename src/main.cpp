ad236e7058771803d6494f88ad588f435c980667
Godlove Damian <godlove@innocent.com>
2019-05-30 16:07:33 +0300

    fixed log print for christmas block

--- src/main.cpp
+++ src/main.cpp
@@ -1293,7 +1293,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
     else if (nHeight < FINAL_REDUCTION_POS_BLOCK_COUNT)
     {
         nSubsidy = nRewardCoinYear * 10 * nCoinAge / 365;
-        fTestNet ? LogPrintf("Calculating 10% reward\n") : LogPrintf("\n") ;
+        LogPrintf("Calculating 10% reward\n");
     }
     else if (nHeight < FLAT_POS_BLOCK_COUNT)
     {
