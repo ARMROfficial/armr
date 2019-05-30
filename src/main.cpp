deef48b872b54937e6d0dc3413ae02ad2f44356d
Godlove Damian <godlove@innocent.com>
2019-05-30 16:07:55 +0300

    fixed log print for final reduction block

--- src/main.cpp
+++ src/main.cpp
@@ -1298,7 +1298,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, const CBlockIndex* pindex)
     else if (nHeight < FLAT_POS_BLOCK_COUNT)
     {
         nSubsidy = nRewardCoinYear * 5 * nCoinAge / 365;
-        fTestNet ? LogPrintf("Calculating 5% reward\n") : LogPrintf("\n") ;
+        LogPrintf("Calculating 5% reward\n");
     }
     else
     {
