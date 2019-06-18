18b521184f863788806c75623cb57cb9c44ca5e6
Godlove Damian <godlove@innocent.com>
2019-06-18 15:48:15 +0300

    included boost member to main.h

--- src/main.h
+++ src/main.h
@@ -27,6 +27,7 @@
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/random_access_index.hpp>
 #include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/member.hpp>
 
 class CWallet;
 class CBlock;
