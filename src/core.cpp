5a27078d1f2ce7af2aaf7a1e8422cc672f197fc6
Godlove Damian <godlove@innocent.com>
2019-06-18 13:08:28 +0300

    removed includes in core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
-#include "core.h"
+
 
 void addAnonOutput(CPubKey& pkAo, CAnonOutput& anonOutput, txMixins_container& txMixinsContainer)
 {
