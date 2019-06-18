8cf19a7f1995c7f9905e9686520c415c0fc4e98d
Godlove Damian <godlove@innocent.com>
2019-06-18 13:13:25 +0300

    added body for method addAnonOutput

--- src/main.cpp
+++ src/main.cpp
@@ -95,7 +95,20 @@ int64_t nMaxAnonStakeOutput = nMaxAnonOutput;
 
 void addAnonOutput(CPubKey& pkAo, CAnonOutput& anonOutput, txMixins_container& txMixinsContainer)
 {
-
+    // create pair with vout index of tx and pubKey
+    const auto & pairOutPubkey = std::make_pair(anonOutput.outpoint.n, pkAo);
+    // check if CTxMixins does already exist in container for tx hash
+    const auto & txHashIndex = txMixinsContainer.get<TXHASH>();
+    const auto it = txHashIndex.find(anonOutput.outpoint.hash);
+    if (it != txHashIndex.end())
+        // add anon pubKey to existing CTxMixins in container
+        it->vOutPubKeys.push_back(pairOutPubkey);
+    else
+    {
+        CTxMixins txMixins(anonOutput.outpoint.hash);
+        txMixins.vOutPubKeys.push_back(pairOutPubkey);
+        txMixinsContainer.push_back(txMixins);
+    }
 }
 
 int64_t PastDrift(int64_t nTime) 
