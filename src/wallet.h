2e8fa05a0d20e6455db8bf484b4147f4628504bc
Godlove Damian <godlove@innocent.com>
2019-06-25 06:01:01 +0300

    created GetAnonStakeAddress function

--- src/wallet.h
+++ src/wallet.h
@@ -228,6 +228,7 @@ public:
 	bool CreateAnonCoinStake(unsigned int nBits, int64_t nSearchInterval, int64_t nFees, CTransaction& txNew, CKey& key);
 
     bool GetAnonChangeAddress(CStealthAddress& sxAddress);
+    bool GetAnonStakeAddress(const COwnedAnonOutput& stakedOao, CStealthAddress& sxAddress);
 
     std::string SendMoney(CScript scriptPubKey, int64_t nValue, std::string& sNarr, CWalletTx& wtxNew, bool fAskFee=false);
     std::string SendMoneyToDestination(const CTxDestination& address, int64 nValue, std::string& sNarr, CWalletTx& wtxNew, bool fAskFee=false);
