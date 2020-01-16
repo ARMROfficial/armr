f7908fec056fbfd8ba6ed6d1b2b0475aabb34e2b
CircuitBreaker88 <sarris88p@gmail.com>
2020-01-16 18:05:46 -0500

    remove mem

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -361,7 +361,7 @@ Value gettxout(const Array& params, bool fHelp)
 
     CTransaction tx;
     uint256 hashBlock = 0;
-    if (!GetTransaction(hash, tx, hashBlock, mem))
+    if (!GetTransaction(hash, tx, hashBlock))
       return Value::null;
 
     if (n<0 || (unsigned int)n>=tx.vout.size() || tx.vout[n].IsNull())
